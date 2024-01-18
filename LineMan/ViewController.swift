2a4e3b0b552927b0a2f3ffee23d87c1f20b2ab6b
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2024-01-18 18:11:27 +0700

    Initial Commit

---
+++ LineMan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LineMan
+//
+//  Created by ReviseUC73 on 18/1/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
